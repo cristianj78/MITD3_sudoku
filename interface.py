fad300bb9783c4c2eebebdbc1da2a3206f5beb65
Abdoul Kader Oke <abdouloke54@gmail.COM>
2025-03-31 22:14:18 +0200

    remplissage des casses du jeu, mise en fonction des butons retour et supprimer
    pour le moment le remplissages casses ne fonction pas correctement il affiche 'Falses' dans toutes les casses du jeu

--- interface.py
+++ interface.py
@@ -1,3 +1,273 @@
+import tkinter as tk
+import random as rd
+import time
+
+
+sudoku = [
+    [
+        [
+            [
+                0,
+                0,
+                0
+            ],
+            [
+                0,
+                0,
+                0
+            ],
+            [
+                0,
+                0,
+                0
+            ],
+        ],
+        [
+            [
+                0,
+                0,
+                0
+            ],
+            [
+                0,
+                0,
+                0
+            ],
+            [
+                0,
+                0,
+                0
+            ],
+        ],
+        [
+            [
+                0,
+                0,
+                0
+            ],
+            [
+                0,
+                0,
+                0
+            ],
+            [
+                0,
+                0,
+                0
+            ],
+        ],
+    ],
+    [
+        [
+            [
+                0,
+                0,
+                0
+            ],
+            [
+                0,
+                0,
+                0
+            ],
+            [
+                0,
+                0,
+                0
+            ],
+        ],
+        [
+            [
+                0,
+                0,
+                0
+            ],
+            [
+                0,
+                0,
+                0
+            ],
+            [
+                0,
+                0,
+                0
+            ],
+        ],
+        [
+            [
+                0,
+                0,
+                0
+            ],
+            [
+                0,
+                0,
+                0
+            ],
+            [
+                0,
+                0,
+                0
+            ],
+        ],
+    ],
+    [
+        [
+            [
+                0,
+                0,
+                0
+            ],
+            [
+                0,
+                0,
+                0
+            ],
+            [
+                0,
+                0,
+                0
+            ],
+        ],
+        [
+            [
+                0,
+                0,
+                0
+            ],
+            [
+                0,
+                0,
+                0
+            ],
+            [
+                0,
+                0,
+                0
+            ],
+        ],
+        [
+            [
+                0,
+                0,
+                0
+            ],
+            [
+                0,
+                0,
+                0
+            ],
+            [
+                0,
+                0,
+                0
+            ]
+        ]
+    ]
+]
+
+i = 0
+j = 0
+k = 0
+l = 0
+retour = 0
+num_possible = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+num_possible_ephemere = {}
+
+def next_number(sudoku,a,b,c,d):
+    """Change la valeur de la case parmi les nombres possible, retourne false si aucune sol n'est trouvé, sinon true"""
+    global retour
+    cle = (a, b, c, d)
+    if not retour:
+        liste_possible_ephemere = num_possible.copy()
+        num_possible_ephemere[cle] = num_possible.copy()
+        
+    else:
+        if sudoku[a][b][c][d] in num_possible_ephemere[cle]:
+            num_possible_ephemere[cle].remove(sudoku[a][b][c][d])
+        liste_possible_ephemere = num_possible_ephemere[cle]
+
+
+    for j in range(3):
+        for l in range(3):
+            if sudoku[a][j][c][l] in liste_possible_ephemere:
+                liste_possible_ephemere.remove(sudoku[a][j][c][l])
+
+    for i in range(3):
+        for k in range(3):
+            if sudoku[i][b][k][d] in liste_possible_ephemere:
+                liste_possible_ephemere.remove(sudoku[i][b][k][d])
+
+    for k in range(3):
+        for l in range(3):
+            if sudoku[a][b][k][l] in liste_possible_ephemere:
+                liste_possible_ephemere.remove(sudoku[a][b][k][l])
+
+
+    num_possible_ephemere[cle] = liste_possible_ephemere
+    if liste_possible_ephemere != []:
+        num_random = rd.choice(liste_possible_ephemere)
+        sudoku[a][b][c][d] = num_random
+        retour = False
+        return True
+            
+        
+
+
+    else:
+        retour = True
+        sudoku[a][b][c][d] = 0
+        return False
+
+
+while i < 3:
+    while j < 3:
+        while k < 3:
+            while l < 3:
+                next = next_number(sudoku,i,j,k,l)
+                if next == False:
+                    if l > 0:
+                        l = l-1
+
+                    elif l == 0 and k > 0:
+                        l = 2
+                        k = k-1
+
+                    elif l == 0 and k == 0 and j > 0:
+                        l = 2
+                        k = 2
+                        j = j-1
+
+                    elif l == 0 and k == 0 and j == 0 and i > 0:
+                        l = 2
+                        k = 2
+                        j = 2
+                        i = i-1
+                else:
+                    if l < 2:
+                        l = l+1
+
+                    elif l == 2 and k < 2:
+                        l = 0
+                        k = k+1
+                        
+                    elif l == 2 and k == 2 and j < 2:
+                        l = 0
+                        k = 0
+                        j = j+1
+                        
+                    elif l == 2 and k == 2 and j == 2 and i < 2:
+                        l = 0
+                        k = 0
+                        j = 0
+                        i = i+1
+                    else:
+                        l = 3
+                        k = 3
+                        j = 3
+                        i = 3
+
+
+
 import tkinter as tk  
 # Creation de la fenetre principale
 fenetre_principal = tk.Tk()
@@ -50,10 +320,11 @@ for t in range (3):
         case.grid(row=t, column=y) # Positionnement des cases
         for o in range (3):
             for p in range(3):
-                button = tk.Button(case, width=4, height=2, bg = "#FFECA1", padx=1, pady=1, font = "Arial", text="")
+                button = tk.Button(case, width=4, height=2, bg = "#FFECA1", padx=1, pady=1, font = "Arial", text=str(next_number(sudoku,t,y,o,p)))
                 button.config(command= lambda bonton = button: placer(bonton, valeur_chiffre))
                 button.grid(row=o, column=p) # Positionnement des boutons
-                
+
+print (button)
 # Creation d'un chronometre
 import time # Importation du module temps
 # Initialisation du temps
@@ -77,17 +348,7 @@ def quitter_partie():
     fenetre_sudoku.destroy() # Ferme la fenetre principale
 button_quiter_partie.config(command=quitter_partie) # Appelle la fenetre pour quitter la partie
 # Creation d'un bouton pour retourner en arriere
-def retour_arriere():
-    pass
-bouton_reture.config(command=retour_arriere) # Appelle la fonction pour retourner en arriere
-# Creation d'un bouton pour supprimer un chiffre
-def supprimer_chiffre(bonton):
-    bontons.remove(bonton)
-    bonton.config(text=" ") # Supprimer le texte du bouton  
-bouton_supprime.config (command=supprimer_chiffre)
     
-bouton_supprime.config(command=supprimer_chiffre) # Appelle la fonction pour supprimer un chiffre
-# Creation d'un bouton pour choisir le niveau facile 
 def niveau_facile():
     pass
 bouton_facile.config(command=niveau_facile) # Appelle la fonction pour choisir le niveau facile 
@@ -118,9 +379,19 @@ def choisir_chiffre(chiffre):
 bontons = []
 def placer(bonton,valeur_chiffre): # Fonction pour changer le texte d'un bouton 
     bonton.config(text=valeur_chiffre) # Change le texte du bouton par le chiffre selectionner 
-    print(button)
     bontons.append(bonton) # Ajouter la fontion placer das la liste des boutons choisies
     print (bontons)
+
+# Fontion pour revenir en arriere
+def revenir_en_arriere(bout):
+    bng = bout.pop()   
+    bng.config(text=" ")
+bout = bontons
+bouton_reture.config(command=lambda: revenir_en_arriere(bout)) # Appelle la fontion pour revenir en arriere
+
+bouton_supprime.config(command=lambda: choisir_chiffre(""))
+
+
 
 
 
